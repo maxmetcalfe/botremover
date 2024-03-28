8ab57bb491921e568c6b6abd9ccd0f4124401e3e
Max Metcalfe <hello@maxmetcalfe.com>
2024-03-27 21:25:52 -0300

    filter on bios

--- botremover.js
+++ botremover.js
@@ -7,29 +7,56 @@
  *   4. Refresh the page
  *   5. The bot followers matching the BOT_USERNAME_REGEX below will be removed.
  *
- *   Warning: Be careful! This may removed non-bot users ;) see regex below.
+ *   Warning: Be careful! This may remove non-bot users ;) see regex below.
  */
 
-// This regex capture all usernames that end with more than 5 numbers (likely bots)
-// You may want to change this based on your followers / aggressiveness
 const BOT_USERNAME_REGEX = /\d{5,}$/;
+const BOT_BIO_REGEXS = [/usdt/gi, /camshat/gi, /letscam/gi, /myfreecontent/gi];
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const removeFollower = async (username, parentElement) => {
+const isBot = (parentElement) => {
+  const link = parentElement.querySelector("a");
+  const username = link.href.split("/").pop();
+
+  if (BOT_USERNAME_REGEX.test(username)) {
+    return true;
+  }
+
+  const bio = parentElement.firstChild.children[1].children[1];
+
+  if (!bio) {
+    return false;
+  }
+
+  const spans = Array.from(bio.getElementsByTagName("span"));
+  const spansText = spans.reduce((acc, curr) => {
+    acc = acc + curr.innerText;
+    return acc;
+  }, "");
+
+  return BOT_BIO_REGEXS.some((regexp) => {
+    return regexp.test(spansText.toLowerCase().replace(/\s/g, ""));
+  });
+};
+
+const removeFollower = async (parentElement) => {
+  const link = parentElement.querySelector("a");
+  const username = link.href.split("/").pop();
+
   try {
     const moreButton = parentElement.querySelector('[aria-label="More"]');
     if (!moreButton) throw new Error(`"More" button not found for ${username}`);
 
     moreButton.click();
-    await delay(100); // Wait for UI to respond
+    await delay(100);
 
     const removeItem = document.querySelector('[data-testid="removeFollower"]');
     if (!removeItem)
       throw new Error(`"Remove" option not found for ${username}`);
 
     removeItem.click();
-    await delay(100); // Wait for confirmation dialog to appear
+    await delay(100);
 
     const confirmButton = document.querySelector(
       '[data-testid="confirmationSheetConfirm"]',
@@ -47,11 +74,7 @@ const removeFollower = async (username, parentElement) => {
 document
   .querySelectorAll('[data-testid="UserCell"]')
   .forEach((followerElement) => {
-    const link = followerElement.querySelector("a");
-    const username = link.href.split("/").pop();
-
-    if (BOT_USERNAME_REGEX.test(username)) {
-      console.log(username);
-      removeFollower(username, followerElement);
+    if (isBot(followerElement)) {
+      removeFollower(followerElement);
     }
   });
